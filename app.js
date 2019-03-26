e67d5744c4df9abc1764f1f9972bbcd6dbab6624
Raja Kannan <sendtorrk@gmail.com>
2019-03-26 14:47:00 -0500

    Switched server to https

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const os = require('os');
 const util = require('util');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 
 const loggingService = require('./services/logging/logging_service');
 const dbService = require('./services/db/db_service');
-
-const app = express();
-const port = 8081;
 
 // Stop
 async function stop() {
@@ -37,6 +37,10 @@ process.on('SIGINT', () => {
   process.exit(0);
 });
 
+// Server
+const app = express();
+const port = 8444;
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -49,17 +53,18 @@ app.use('/api/v1', require('./routes/v1/assetnotes_routes'));
 // NOTE: Error handler should always be the last middleware. DO NOT add any
 // routes after error handler.
 app.use((err, req, res, next) => {
-  // Errors MUST be handled by higher level code. This last chance error
-  // handler insures the client gets a response to the request. But if an
-  // error propagates all the way to this handler it is considered an internal
-  // server error (status code 500).
   if (err.stack) {
     loggingService.error(err.stack);
   }
 
-  loggingService.error(err);
+  loggingService.error(http.STATUS_CODES[500] + ': ' + err);
   return res.status(500).json(err);
 });
+
+const appServer = https.createServer({
+  key: fs.readFileSync('crt/SatKey.key', 'utf8'),
+  cert: fs.readFileSync('crt/SatCert.crt', 'utf8'),
+}, app);
 
 // Start
 async function start() {
@@ -67,7 +72,7 @@ async function start() {
     loggingService.info('Starting server ...');
 
     await dbService.synchronizeDb();
-    await app.listen(port);
+    await appServer.listen(port);
 
     loggingService.info(`Server started and listening on port ${port}`);
   }
